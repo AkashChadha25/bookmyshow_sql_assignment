763ec2c1c9999a6b1aa66db7c3c28ca101fb07f1
AkashChadha25 <chadhaakash134@gmail.com>
2025-06-07 19:42:20 +0530

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -1,13 +1,12 @@
-
 -- Query to fetch all shows on a given date and theatre
 SELECT 
     M.title AS movie_title,
     M.language,
     M.certification,
     M.format,
-    S.show_time
+    S.Show_time
 FROM 
-    Show S
+    Show_Details S
 JOIN 
     Movie M ON S.movie_id = M.movie_id
 JOIN 
@@ -18,4 +17,4 @@ WHERE
     S.show_date = '2023-04-25'
     AND T.name = 'PVR: Nexus'
 ORDER BY 
-    S.show_time;
+    S.Show_time;
